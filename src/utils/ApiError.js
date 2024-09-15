008a7482b655ba16b8f8b0e4c461853f19bfc033
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-09-15 06:50:23 +0500

    creatig a ApiError & ApiRespose files

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Somethin went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    (this.statusCode = statusCode),
+      (this.errors = errors),
+      (this.message = message),
+      (this.success = false);
+      if(stack){
+        this.stack = stack
+      }else{
+        Error.captureStackTrace(this, this.constructor)
+      }
+  }
+}
+
+export {ApiError}
